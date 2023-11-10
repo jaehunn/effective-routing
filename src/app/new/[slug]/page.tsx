51b9d87c37decb674e77311b05f0f1b758c9dbe9
Jaehun <qkdwogns98@gmail.com>
2023-11-10 23:23:08 +0900

    chore: @testing-library/jest 타입 정의 추가, @testing-library/dom 설치

--- src/app/new/[slug]/page.tsx
+++ src/app/new/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import SignUpPage from './SignUpPage';
 import PlanPage from './PlanPage';
 import AddonsPage from './AddonsPage';
+import WrapUpPage from './WrapUpPage';
+import DonePage from './DonePage';
 
 export const STEPS = {
   /** 구독 신청자 정보 입력 */
@@ -15,10 +17,10 @@ export const STEPS = {
   addons: <AddonsPage />,
 
   /** 구독 신청 정보 요약 */
-  'wrap-up': <div>wrap-up</div>,
+  'wrap-up': <WrapUpPage />,
 
   /** 구독 완료 */
-  done: <div>done</div>,
+  done: <DonePage />,
 };
 
 export type Params = {
